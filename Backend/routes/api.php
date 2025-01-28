c9099fa966273a78e48d691e2e549f2f28622e49
eerr00rr <alex.11.monti@gmail.com>
2025-01-28 12:54:17 +0100

    test login from angular

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApiController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 
 Route::get('/user', function (Request $request) {
@@ -10,10 +11,8 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::middleware('guest')->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
-        ->name('register');
-
     Route::post('register', [RegisteredUserController::class, 'store']);
+    Route::post('login', [AuthenticatedSessionController::class, 'store']);
 });
 
 Route::get('/todolists', [ApiController::class, 'getTodolists']);
